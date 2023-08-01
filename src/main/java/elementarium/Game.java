72ff804b7e7bc26c8252011108f71a1a166279f4
cognaiger <21020369@vnu.edu.vn>
2023-08-01 23:08:59 +0700

    starter

--- src/main/java/elementarium/Game.java
+++ src/main/java/elementarium/Game.java
@@ -1,19 +1,20 @@
 package elementarium;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Game extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 800, 600);
+
         stage.setScene(scene);
+        stage.setTitle("Elementarium");
         stage.show();
     }
 
