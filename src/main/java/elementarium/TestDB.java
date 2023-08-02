355531a21b32fc60f6bc2b0b72ac0a69f8ffed21
Thanh Tung <21020399@vnu,edu.vn>
2023-08-02 21:44:22 +0700

    pull master

--- src/main/java/elementarium/TestDB.java
+++ src/main/java/elementarium/TestDB.java
@@ -8,10 +8,11 @@ import java.sql.SQLException;
 public class TestDB {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         MySQLConnection mySQLConnection = new MySQLConnection();
-        String sql = "SELECT COUNT(*) FROM element";
+        String sql = "SELECT element_name FROM element";
         ResultSet rs = mySQLConnection.executeQuery(sql);
-        rs.next();
-        int count = rs.getInt(1);
-        System.out.println(count);
+        while (rs.next()) {
+            String elementName = rs.getString("element_name");
+            System.out.println("Element Name: " + elementName);
+        }
     }
 }
